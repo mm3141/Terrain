a07eb0f21597f9f307613c22d91ab25ae32cc02c
MM <meyerm1@gmail.com>
2020-07-14 19:28:53 +0200

    Revert "move offsets to plugin"
    This reverts commit 0cf35450625b9f19f802256d07773f76716b892c.

--- src/TerrainCore.cs
+++ src/TerrainCore.cs
@@ -4,13 +4,11 @@ using ExileCore;
 using ExileCore.PoEMemory.Components;
 using ExileCore.PoEMemory.MemoryObjects;
 using ExileCore.Shared.Cache;
-using ExileCore.Shared.Helpers;
-using GameOffsets.Native;
 using SharpDX;
 using SharpDX.Direct3D11;
 using SharpDX.DXGI;
-using Map = ExileCore.PoEMemory.Elements.Map;
-using RectangleF = SharpDX.RectangleF;
+
+using Map=ExileCore.PoEMemory.Elements.Map;
 
 namespace Terrain
 {
@@ -59,10 +57,8 @@ namespace Terrain
 		{
 			if (!Settings.Enable)
 				return;
-			var data = GameController.IngameState.Data;
-			var terrain = data.M.Read<TerrainData>(data.Address + 0x608);
-
-			var terrainBytes = GameController.Memory.ReadBytes(terrain.Layer1.First, terrain.Layer1.Size);
+			var terrain = GameController.IngameState.Data.Terrain;
+			var terrainBytes = GameController.Memory.ReadBytes(terrain.LayerMelee.First, terrain.LayerMelee.Size);
 			
 			_numCols = (int) terrain.NumCols * 23;
 			_numRows = (int) terrain.NumRows * 23;
@@ -150,12 +146,12 @@ namespace Terrain
 
 			Vector2 Transform(Vector2 p) => _screenCenterCache + DeltaInWorldToMinimapDelta(p - playerPos, Diag, _scale, -posZ / (9f / mapWindowLargeMapZoom));
 
-			Graphics.LowLevel.ImGuiRender.LowLevelApi.AddImageQuad(Graphics.LowLevel.GetTexture("terrain").NativePointer,
-				Transform(new Vector2(0, 0)).ToVector2Num(),
-				Transform(new Vector2(_numCols, 0)).ToVector2Num(),
-				Transform(new Vector2(_numCols, _numRows)).ToVector2Num(),
-				Transform(new Vector2(0, _numRows)).ToVector2Num()
-				);
+			Graphics.DrawTexture(Graphics.LowLevel.GetTexture("terrain").NativePointer,
+				Transform(new Vector2(0, 0)),
+				Transform(new Vector2(_numCols, 0)),
+				Transform(new Vector2(_numCols, _numRows)),
+				Transform(new Vector2(0, _numRows))
+			);
 		}
 
 		public Vector2 DeltaInWorldToMinimapDelta(Vector2 delta, double diag, float scale, float deltaZ = 0)
@@ -169,16 +165,6 @@ namespace Terrain
 
 			// 2D rotation formulas not correct, but it's what appears to work?
 			return new Vector2((delta.X - delta.Y) * cos, deltaZ - (delta.X + delta.Y) * sin);
-		}
-
-		[StructLayout(LayoutKind.Explicit, Pack = 1)]
-		public struct TerrainData
-		{
-			[FieldOffset(0x18)] public long NumCols;
-			[FieldOffset(0x20)] public long NumRows;
-			[FieldOffset(0xd8)] public NativePtrArray Layer1;
-			[FieldOffset(0xf0)] public NativePtrArray Layer2;
-			[FieldOffset(0x108)] public int BytesPerRow;
 		}
 	}
 }
